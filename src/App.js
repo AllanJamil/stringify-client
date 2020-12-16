fcedaa1abc02d0f01f0e231aeabffd3a0d460ffd
Allan <allanjamil_91@hotmail.com>
2020-12-16 18:58:59 +0100

    test websocket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Stringify Client
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [count, setCount] = useState(45);
+    const [name, setName] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    let socket = useRef(null);
+    let stompClient = useRef(null);
+
+    useEffect(() => {
+
+        const connect =  (frame) => {
+            stompClient.current.subscribe("/topic/count", function (messageCount) {
+                let message = JSON.parse(messageCount.body);
+                console.log(message);
+                setCount(message.count);
+                messages.push(message)
+                setMessages(messages);
+            });
+        };
+        socket.current = new SockJS("https://stringify-chat.herokuapp.com/ws");
+        stompClient.current = Stomp.over(socket.current);
+
+        stompClient.current.connect({}, connect ,
+            function () {
+                setTimeout(connect, 500);
+                console.log("fail");
+
+            });
+
+    },[messages]);
+
+
+
+
+
+    const increment = () => {
+        let messageCount = {
+            name, count
+        };
+        stompClient.current.send("/app/increase",{} ,JSON.stringify(messageCount));
+    }
+
+
+    const renderMessageCount = messages.map(message => {
+        return (
+            <tr key={message.count}>
+                <td style={getStyle()} >{message.name}</td>
+                <td style={getStyle()} >{message.count}</td>
+            </tr>
+        );
+    });
+
+    function getStyle() {
+        return {border: "1px solid black"};
+    }
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+
+            <label htmlFor="name">Name</label>
+            <br/>
+
+            <input id="name" value={name} type="text" onChange={event => setName(event.target.value)}/>
+
+            <button onClick={() => increment()} >Increment</button>
+
+            <br/>
+            <br/>
+            <br/>
+            <table style={getStyle()}>
+                <thead>
+                    <tr>
+                        <th style={getStyle()} >Name</th>
+                        <th style={getStyle()} >Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderMessageCount}
+                </tbody>
+            </table>
+        </div>
+
+    );
+};
 
 export default App;
