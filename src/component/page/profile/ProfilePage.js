399de65b4fa4a12fe11ca37ff1d38a0bfc6dab51
Allan Jamil <allanjami_91@hotmail.com>
2020-12-19 03:09:29 +0100

    added info to ProfilePage.js

--- src/component/page/profile/ProfilePage.js
+++ src/component/page/profile/ProfilePage.js
@@ -15,7 +15,17 @@ const ProfilePage = () => {
     return (
         <div style={getStyle}>
             <div style={{textAlign: "left"}} className="top-line">Almost there!</div>
-            <h1 style={{marginBottom: "50px", textAlign: "left"}}>Create Your Profile</h1>
+            <h1 style={{marginBottom: "10px", textAlign: "left"}}>Create Your Profile</h1>
+            <p
+                style={{
+                    marginBottom: "35px",
+                    maxWidth: "440px",
+                    fontSize: "18px",
+                    textAlign: "left"
+                }}
+            >
+                Enter your name to connect. You can also pick a avatar to your liking!
+            </p>
             <AvatarSlider/>
             <NameField/>
         </div>
