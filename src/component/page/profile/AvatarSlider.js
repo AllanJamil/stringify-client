40ae9a56a63dc6a911fb2be7e128a483f9396900
Allan <allanjamil_91@hotmail.com>
2020-12-21 22:37:48 +0100

    more avatars

--- src/component/page/profile/AvatarSlider.js
+++ src/component/page/profile/AvatarSlider.js
@@ -24,6 +24,7 @@ import avatar18 from '../../../images/avatars/avatar18.png';
 import avatar19 from '../../../images/avatars/avatar19.png';
 import avatar20 from '../../../images/avatars/avatar20.png';
 
+
 const images = [
     avatar1,
     avatar2,
