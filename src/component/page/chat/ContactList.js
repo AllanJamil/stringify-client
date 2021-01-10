8696fc63b7d098916a225ea17e082c8a817fb06c
Allan Jamil <allanjami_91@hotmail.com>
2021-01-10 10:35:54 +0100

    most parts of the client are completed. Clean up, refactoring and connecting to server remains.

--- src/component/page/chat/ContactList.js
+++ src/component/page/chat/ContactList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContactDetail from "./ContactDetail";
-import './ChatPage.css';
+import './MeetingPage.css';
 
 const ContactList = ({device, contacts, theme}) => {
 
